e7d4269df7872e7d49b71141831ab70caf240b6d
NefeliLefa <123506734+NefeliLefa@users.noreply.github.com>
2023-05-21 22:58:32 +0200

    final update

--- yesno.py
+++ yesno.py
@@ -1,21 +1,6 @@
-import tkinter as tk
-import tkinter.colorchooser as col
-import tkinter.simpledialog as sd
 from tkinter import messagebox
-
-
-class myGraph():
-
-    def __init__(self, root):
-        self.root = root
-      
-
-    
-
+  
 def main():
-    root = tk.Tk()
-    game = myGraph(root)
-    root.mainloop()
     answer = messagebox.askyesno(title="Pregunta", message="Is the Earth flat?")
     if answer == True:
         print('Flat Earther')
